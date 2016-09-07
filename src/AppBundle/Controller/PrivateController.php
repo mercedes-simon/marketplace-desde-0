27e5c8d3ca2712e21ca016960fe3978e671c269b
Mercedes <merchesimon@gmail.com>
2016-09-07 09:47:49 +0000

    Terminado el punto 7.4

---
+++ src/AppBundle/Controller/PrivateController.php
@@ -0,0 +1,48 @@
+<?php
+namespace AppBundle\Controller;
+use AppBundle\Entity\Trayecto;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PrivateController extends Controller
+
+{
+    /**
+     * @Route("/nuevoTrayecto", name="private_nuevoTrayecto")
+     */
+    public function nuevoTrayectoAction(Request $request)
+    {
+        return $this->render('nuevoTrayecto/index.html.twig');
+    }
+
+    /**
+     * @Route("/publicarTrayecto", name="private_publicarTrayecto")
+     */
+    public function PublicarTrayectoAction(Request $request)
+    {
+        $nuevoTrayecto = new Trayecto();
+        $nuevoTrayecto->setOrigen($request->get("origen"));
+        $nuevoTrayecto->setDestino($request->get("destino"));
+        $nuevoTrayecto->setCalle($request->get("calle"));
+        $fechaDateTime = new \DateTime($request->get("fechaDeViaje"));
+        $nuevoTrayecto->setFechaDeViaje($fechaDateTime);
+        $horaDateTime = new \DateTime($request->get("horaDeViaje"));
+        $nuevoTrayecto->setHoraDeViaje($horaDateTime);
+        $nuevoTrayecto->setPrecio($request->get("precio"));
+        $nuevoTrayecto->setDescripcion($request->get("descripcion"));
+        $nuevoTrayecto->setPlazas($request->get("plazas"));
+        $usuarioLogueado = $this->getUser();
+        $nuevoTrayecto->setConductor($usuarioLogueado);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($nuevoTrayecto);
+        $entityManager->flush();
+        
+    {
+        return $this->redirect($this->generateUrl("public_home"));
+    }
+    }
+
+
+       
+}
